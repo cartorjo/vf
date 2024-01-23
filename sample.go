81ee2e09246af294f7c6e061efd20d187425f9ec
José <jose@mlab.es>
2024-01-23 20:25:38 +0100

    Update sample.go

--- sample.go
+++ sample.go
@@ -2,15 +2,26 @@
 
 package main
 
-import "fmt"
-
-func greet(name string) string {
-    return fmt.Sprintf("Hello, %s!", name)
-}
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-    personName := "Jose"
-    greeting := greet(personName)
+	// Simulate a potential security issue: executing a shell command with user input
+	userInput := os.Args[1]
+	command := fmt.Sprintf("echo %s", userInput)
 
-    fmt.Println(greeting)
+	cmd := exec.Command("sh", "-c", command)
+
+	// Execute the command
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error executing command:", err)
+		return
+	}
+
+	// Print the command output
+	fmt.Println("Command output:", string(output))
 }
